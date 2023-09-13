057dd3c8afb79a0c689f9037d16b31f4d69c87c5
Shayne Geilman <shaynegeilman@gmail.com>
2023-09-13 14:37:47 +0800

    imageblock css applied

--- SevenCode/src/components/ImageBlock.tsx
+++ SevenCode/src/components/ImageBlock.tsx
@@ -1,10 +1,12 @@
 import { ImageBlockProps } from "../types/types";
-
+import styles from "../styles/imageBlock.module.css";
 export default function ImageBlock({ data }: { data: ImageBlockProps }) {
   return (
     <figure>
-      <img src={data.url} alt="image failed to load" />
-      {data.captionText && <figcaption>{data.captionText}</figcaption>}
+      <img className={styles.image} src={data.url} alt="image failed to load" />
+      {data.captionText && (
+        <figcaption className={styles.caption}>{data.captionText}</figcaption>
+      )}
     </figure>
   );
 }
